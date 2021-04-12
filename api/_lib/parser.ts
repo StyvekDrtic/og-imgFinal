c44e6a9b623b458361fc4e760d3d702438752cc6
StyvekDrtic <jiristepanek@hotmail.com>
2021-04-12 22:05:26 +0200

    zeby ted

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -1,28 +1,17 @@
 import { IncomingMessage } from 'http';
 import { parse } from 'url';
-import { ParsedRequest, Theme } from './types';
+import { ParsedRequest } from './types';
 
 export function parseRequest(req: IncomingMessage) {
     console.log('HTTP ' + req.url);
-    const { pathname, query } = parse(req.url || '/', true);
+    const { query } = parse(req.url || '/', true);
     const { zesnuly, datum, image } = (query || {});
 
-    const arr = (pathname || '/').slice(1).split('.');
-    let extension = '';
-    let text = '';
-    if (arr.length === 0) {
-        text = '';
-    } else if (arr.length === 1) {
-        text = arr[0];
-    } else {
-        extension = arr.pop() as string;
-        text = arr.join('.');
-    }
 
     const parsedRequest: ParsedRequest = {
-        zesnuly: zesnuly,
-        datum: datum,
-        image: image,
+        zesnuly: zesnuly!,
+        datum: datum!,
+        image: image!,
     };
 
     return parsedRequest;
